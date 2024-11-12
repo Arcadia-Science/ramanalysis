32dfb108032b3ad3235926d26156153251e4bd82
lanery <ryanilane@gmail.com>
2024-11-12 10:01:56 -0800

    Update peak_fitting.py
    - add parabolic fit function for refining peak positions

--- ramanalysis/peak_fitting.py
+++ ramanalysis/peak_fitting.py
@@ -18,29 +18,87 @@ def gaussian(x: ScalarArray, amplitude: float, mean: float, stddev: float) -> Fl
     return amplitude * np.exp(-((x - mean) ** 2) / (2 * stddev**2))
 
 
-def refine_peak(peak: int, signal: FloatArray, window_size: int = 11) -> float:
+def refine_peak_gaussian_fit(
+    int_peak_index: int, signal: FloatArray, window_size: int = 7
+) -> float:
     """Refine an estimated peak position in a 1D signal by least-squares Gaussian fit.
 
     Args:
-        peak: Integer index position of `signal` at which a peak is expected.
+        int_peak_index: Integer index position of `signal` at which a peak is expected.
         signal: Input one-dimensional signal with which to refine the peak position.
         window_size: Window size around the peak for Gaussian fit.
+
+    Returns:
+        i_peak: subpixel-interpolated peak position.
     """
-    peak = int(peak)
-    x_window_start, x_window_end = (peak - window_size // 2, peak + window_size // 2 + 1)
+    i = int(int_peak_index)
+    x_window_start, x_window_end = (i - window_size // 2, i + window_size // 2)
     x_window = np.arange(x_window_start, x_window_end)
-    initial_guesses = [1, peak, 1]
+    initial_guesses = [1, i, 1]
     fit_parameters, _covariance_matrix = curve_fit(
         gaussian, xdata=x_window, ydata=signal[x_window], p0=initial_guesses
     )
-    return fit_parameters[1]
+    i_peak = fit_parameters[1]
+    return i_peak
 
 
-def refine_peaks(peaks: IntArray, signal: FloatArray, window_size: int = 11) -> FloatArray:
-    """Applies :func:`refine_peak` to an arbitrary number of peaks."""
+def refine_peak_parabolic_fit(int_peak_index: int, signal: FloatArray) -> float:
+    """Refine an estimated peak position in a 1D signal by a parabolic fit.
+
+    Uses the formula
+        i_peak = i + (y_{i-1} - y_{i+1}) / (2 * (y_{i-1} - 2y_i + y_{i+1}))
+    where y_{i-1}, y_i, and y_{i+1} are the values of the signal at indices i-1, i, and i+1.
+    Assuming the peak is roughly centered around i, the parabola is fit around these three points.
+
+    Args:
+        int_peak_index: Integer index position of `signal` at which a peak is expected.
+        signal: Input one-dimensional signal with which to refine the peak position.
+
+    Returns:
+        i_peak: subpixel-interpolated peak position.
+    """
+    # aliases to match the formula
+    i = int(int_peak_index)
+    y_left = signal[i - 1]
+    y_center = signal[i]
+    y_right = signal[i + 1]
+    # avoid division by 0
+    if y_left + y_right == 2 * y_center:
+        logger.warning("Peak position could not be refined.")
+        return i
+    # apply formula
+    i_peak = i + (y_left - y_right) / (2 * (y_left - 2 * y_center + y_right))
+    return i_peak
+
+
+def refine_peaks(
+    peaks: IntArray,
+    signal: FloatArray,
+    method: str = "parabolic",
+    **kwargs,
+) -> FloatArray:
+    """Applies :func:`refine_peak` to an arbitrary number of peaks.
+
+    Uses parabolic fitting by default as any decrease in accuracy is made up for by the increase
+    in simplicity and reliability.
+
+    Args:
+        peaks: Integer indices of peak positions in the signal.
+        signal: Input one-dimensional signal with which to refine the peak positions.
+        method: Whether to refine peaks by fitting a Gaussian distribution or a parabola.
+    """
+    if method.lower().startswith("gauss"):
+        refine_peak = refine_peak_gaussian_fit
+    elif method.lower().startswith("parabo"):
+        refine_peak = refine_peak_parabolic_fit
+    else:
+        raise ValueError(
+            "Invalid choice for peak refinement. Must be one of `gaussian` or `parabolic`."
+        )
+
     refined_peaks: list[float] = []
     for peak in peaks:
-        refined_peak = refine_peak(peak, signal, window_size)
+        refined_peak = refine_peak(peak, signal, **kwargs)
         refined_peaks.append(refined_peak)
     return np.array(refined_peaks)
 
@@ -58,7 +116,7 @@ def find_n_most_prominent_peaks(
     specified number of peaks remains.
 
     Args:
-        signal: Input one-dimensional signal from which to find peaks
+        signal: Input one-dimensional signal from which to find peaks.
         num_peaks: Specified number of peaks to find.
         prominence_increment: Increment for prominence value in the optimization loop.
         max_iterations: Max number of iterations for the optimization loop.
@@ -70,7 +128,7 @@ def find_n_most_prominent_peaks(
     """
     i = 0
     prominence = 0
-    peaks, _ = find_peaks(signal, prominence=prominence)
+    peaks, _properties = find_peaks(signal, prominence=prominence)
     num_peaks_found = int(peaks.size)  # type: ignore
     if num_peaks_found < num_peaks:
         message = (

e4ab9b909f9a94af685b632a56473a516c23220b
lanery <ryanilane@gmail.com>
2024-11-12 10:03:25 -0800

    Create conftest.py

---
+++ ramanalysis/tests/conftest.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+from natsort import natsorted
+
+
+@pytest.fixture
+def test_data_filepath():
+    return Path(__file__).parent / "example_data"
+
+
+@pytest.fixture
+def valid_openraman_csv_filepath(test_data_filepath):
+    return test_data_filepath / "OpenRAMAN" / "2024-10-04_CC-124-TAP_n_n_solid_10000_0_5.csv"
+
+
+@pytest.fixture
+def valid_openraman_excitation_csv_filepath(test_data_filepath):
+    return test_data_filepath / "OpenRAMAN" / "2024-10-04_neon_n_n_solid_10000_0_5.csv"
+
+
+@pytest.fixture
+def valid_openraman_emission_csv_filepath(test_data_filepath):
+    return test_data_filepath / "OpenRAMAN" / "2024-10-04_acetonitrile_n_n_solid_10000_0_5.csv"
+
+
+@pytest.fixture
+def valid_horiba_txt_filepath(test_data_filepath):
+    return test_data_filepath / "Horiba" / "polystyrene.txt"
+
+
+@pytest.fixture
+def valid_synthetic_spectrum_wavenumbers(test_data_filepath):
+    valid_synthetic_spectrum_filepath = (
+        test_data_filepath / "synthetic_data" / "fake_spectrum_0.txt"
+    )
+    synthetic_wavenumbers_cm1 = np.loadtxt(valid_synthetic_spectrum_filepath)[:, 0]
+    return synthetic_wavenumbers_cm1
+
+
+@pytest.fixture
+def valid_synthetic_spectrum_intensities(test_data_filepath):
+    valid_synthetic_spectrum_filepath = (
+        test_data_filepath / "synthetic_data" / "fake_spectrum_0.txt"
+    )
+    synthetic_intensities = np.loadtxt(valid_synthetic_spectrum_filepath)[:, 1]
+    return synthetic_intensities
+
+
+@pytest.fixture
+def batch_neon_spectra_csv_filepaths(test_data_filepath: Path):
+    """File paths to a batch of neon spectra independently acquired over several weeks."""
+    calibration_data_filepath = test_data_filepath / "OpenRAMAN" / "calibration_data"
+    batch_neon_spectra_csv_filepaths = natsorted(calibration_data_filepath.glob("*neon*.csv"))
+    return batch_neon_spectra_csv_filepaths
+
+
+@pytest.fixture
+def batch_acetonitrile_spectra_csv_filepaths(test_data_filepath: Path):
+    """File paths to a batch of acetonitrile spectra independently acquired over several weeks."""
+    calibration_data_filepath = test_data_filepath / "OpenRAMAN" / "calibration_data"
+    batch_acetonitrile_spectra_csv_filepaths = natsorted(
+        calibration_data_filepath.glob("*aceto*.csv")
+    )
+    return batch_acetonitrile_spectra_csv_filepaths
